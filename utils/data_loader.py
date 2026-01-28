1befda853a9caa2403b8fe3a6f03e2662790d276
sharminishar5 <sharminishar5@gmail.com>
2026-01-28 23:26:48 +0800

    Fix formatting issues in data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -4,7 +4,9 @@ import streamlit as st
 @st.cache_data
 def load_data():
     df = pd.read_csv("data/cleaned_youtube_comments.csv")
-    df['Cleaned_Comment'] = df['Cleaned_Comment'].fillna("")
-    df['Actual_Sentiment'] = df['Actual_Sentiment'].fillna("Unknown")
-    df['Comment_Length'] = df['Cleaned_Comment'].apply(len)
+
+    df["Cleaned_Comment"] = df["Cleaned_Comment"].fillna("")
+    df["Actual_Sentiment"] = df["Actual_Sentiment"].fillna("Unknown")
+    df["Comment_Length"] = df["Cleaned_Comment"].apply(len)
+
     return df
